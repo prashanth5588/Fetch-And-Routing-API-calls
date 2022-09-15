e5e72477866eba371ecc55e6f38d09a53fe4b57c
prashanth <prashantht355@gmail.com>
2022-09-15 06:10:13 +0000

    fetching-routing

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+import Header from './components/Header/index'
+import About from './components/About/index'
+import Home from './components/Home/index'
+import Contact from './components/Contact/index'
+import BlogItemDetails from './components/BlogItemDetails/index'
+import NotFound from './components/NotFound/index'
+
+import './App.css'
+
+const App = () => (
+  <div className="app-container">
+    <div className="responsive-container">
+      <BrowserRouter>
+        <Header />
+        <div className="app-body">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route path="/blogs/:id" component={BlogItemDetails} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  </div>
+)
+
+export default App
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import BlogList from '../BlogList/index'
+
+import UserInfo from '../UserInfo/index'
+
+import './index.css'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogList />
+  </div>
+)
+
+export default Home
